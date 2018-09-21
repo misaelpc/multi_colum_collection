f13c81ef50cf54a90a84ab36fd3bedd347437150
Misael Pérez Chamorro <misaelpcgm@gmail.com>
2018-09-20 19:11:34 -0500

    Initial Commit

---
+++ MultiColum/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultiColum
+//
+//  Created by Misael Pérez Chamorro on 9/20/18.
+//  Copyright © 2018 Misael Pérez Chamorro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
